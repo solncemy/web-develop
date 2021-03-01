17e3facfd507d1a78cfb86bd956d9319d8e68559
solncemy <79795333+solncemy@users.noreply.github.com>
2021-03-01 10:26:47 +0300

    Create task1.pas

---
+++ lw2/task1.pas
@@ -0,0 +1,12 @@
+PROGRAM PrintHello(INPUT, OUTPUT);
+USES
+  dos;
+BEGIN
+  WRITELN('Content-Type: text/plain');
+  WRITELN;
+  WRITELN(GetEnv('REQUEST_METHOD'));
+  WRITELN(GetEnv('QUERY_STRING'));
+  WRITELN(GetEnv('CONTENT_LENGTH'));
+  WRITELN(GetEnv('HTTP_USER_AGENT'));
+  WRITELN(GetEnv('HTTP_HOST'))
+END.
